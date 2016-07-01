973d0a5d16b8b193028dfefa936de4c2e404344a
Qin Jianxiang <talkativedoggy@gmail.com>
2016-07-01 11:09:53 +0800

    Command adapter worked

--- adapter/Command.go
+++ adapter/Command.go
@@ -1,19 +1,21 @@
 package adapter
 
 import (
-	"os/exec"
-	"github.com/OpsKitchen/ok_agent/util"
+	"bufio"
 	"errors"
+	"github.com/OpsKitchen/ok_agent/util"
+	"io"
 	"os"
+	"os/exec"
 )
 
 type Command struct {
-	Command string
-	Cwd     string
-	Path    string
-	User    string
-	OnlyIf  string
-	Unless  string
+	Command  string
+	Cwd      string
+	Path     string
+	User     string
+	RunIf    string
+	NotRunIf string
 }
 
 func (item *Command) Process() error {
@@ -35,30 +37,78 @@ func (item *Command) Process() error {
 		} else if stat.IsDir() == false {
 			util.Logger.Error("Cwd is not a directory: ", item.Cwd)
 			return errors.New("Cwd is not a directory: " + item.Cwd)
-		}//@todo else if item.User has no permission on this dir?
+		}
 	}
 
 	//check if necessary to run command
-	if item.OnlyIf != "" && item.check(item.OnlyIf) == false {
+	if item.RunIf != "" && item.fastRun(item.RunIf) == false {
 		return nil
 	}
-	if item.Unless != "" && item.check(item.Unless) == true {
+	if item.NotRunIf != "" && item.fastRun(item.NotRunIf) == true {
 		return nil
 	}
 
 	//run command
-	util.Logger.Info("Runing command: ", item.Command)
-	return nil
+	return item.runWithOutput()
 }
 
-func (item *Command) check(command string) bool {
+func (item *Command) fastRun(command string) bool {
 	var cmd *exec.Cmd
 	var err error
 	cmd = exec.Command("/bin/sh", "-c", command)
-	item.setCwd(cmd)
 	item.setPath(cmd)
 	err = cmd.Run()
 	return err == nil
+}
+
+func (item *Command) runWithOutput() error {
+	var cmd *exec.Cmd
+	var err error
+	var errPipe, outPipe io.ReadCloser
+	var errReader, outReader *bufio.Reader
+	var line string
+
+	//prepare cmd object
+	cmd = exec.Command("/bin/sh", "-c", item.Command)
+	item.setCwd(cmd)
+	item.setPath(cmd)
+
+	outPipe, _ = cmd.StdoutPipe()
+	errPipe, _ = cmd.StderrPipe()
+	err = cmd.Start()
+	if err != nil {
+		util.Logger.Error("Can not start command: ", item.Command)
+		return err
+	} else {
+		util.Logger.Info("Running command: ", item.Command)
+	}
+
+	//real-time output of std out
+	outReader = bufio.NewReader(outPipe)
+	for {
+		line, err = outReader.ReadString('\n')
+		if err != nil || io.EOF == err {
+			break
+		}
+		util.Logger.Debug(line)
+	}
+
+	//real-time output of std err
+	errReader = bufio.NewReader(errPipe)
+	for {
+		line, err = errReader.ReadString('\n')
+		if err != nil || io.EOF == err {
+			break
+		}
+		util.Logger.Error(line)
+	}
+	err = cmd.Wait()
+	if err != nil {
+		util.Logger.Error("Command finished, but error occourred: ", item.Command)
+		return err
+	} else {
+		return nil
+	}
 }
 
 func (item *Command) setCwd(cmd *exec.Cmd) {
@@ -70,5 +120,7 @@ func (item *Command) setCwd(cmd *exec.Cmd) {
 func (item *Command) setPath(cmd *exec.Cmd) {
 	if item.Path != "" {
 		cmd.Env = append(cmd.Env, "PATH="+item.Path)
+	} else {
+		cmd.Env = append(cmd.Env, "PATH="+os.Getenv("PATH"))
 	}
 }
