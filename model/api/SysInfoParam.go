371c9c27bcf33115710ecf2a6ebfba00777895f6
Qin Jianxiang <talkativedoggy@gmail.com>
2016-07-26 12:42:38 +0800

    remove unused field

--- model/api/SysInfoParam.go
+++ model/api/SysInfoParam.go
@@ -1,10 +1,9 @@
 package api
 
 type SysInfoParam struct {
-	ServerUniqueName string `json:"serverUniqueName"`
-	MachineType      string `json:"machineType"`
-	Hostname         string `json:"hostname"`
-	Cpu              int    `json:"cpu"`
-	Memory           int    `json:"memory"`
-	Ip               string `json:"ip"`
+	MachineType string `json:"machineType"`
+	Hostname    string `json:"hostname"`
+	Cpu         int    `json:"cpu"`
+	Memory      int    `json:"memory"`
+	Ip          string `json:"ip"`
 }
