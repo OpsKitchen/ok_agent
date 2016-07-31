ace6c6efb8b5cf427ca75cc96c62057d99adba4a
Qin Jianxiang <talkativedoggy@gmail.com>
2016-07-31 16:35:33 +0800

    write log into file, worked

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/OpsKitchen/ok_agent/util"
 	"github.com/OpsKitchen/ok_api_sdk_go/sdk"
 	"github.com/Sirupsen/logrus"
+	"os"
 	"time"
 )
 
@@ -27,10 +28,28 @@ func main() {
 	}
 
 	//prepare config
-	baseConf := &config.Base{}
+	baseConf := &config.Base{LogDir: "/var/log/ok_agent"}
 	if err := util.ParseJsonFile(*baseConfigFile, baseConf); err != nil {
 		util.Logger.Fatal("Failed to parse base config file: " + err.Error())
 	}
+
+	//check log dir
+	if _, err := os.Stat(baseConf.LogDir); err != nil { //log dir not exists
+		if os.MkdirAll(baseConf.LogDir, 0755) != nil {
+			util.Logger.Fatal("Failed to create log dir [" + baseConf.LogDir + "]: " + err.Error())
+		}
+	}
+	logFileName := baseConf.LogDir + "/info.log"
+	var fileHandle *os.File
+	if _, err := os.Stat(logFileName); err != nil { //log file not exists
+		if fileHandle, err = os.OpenFile(logFileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666); err != nil {
+			util.Logger.Fatal("Failed to create log file [" + logFileName + "]: " + err.Error())
+		}
+	} else if fileHandle, err = os.OpenFile(logFileName, os.O_WRONLY|os.O_APPEND, 0666); err != nil {
+		util.Logger.Fatal("Failed to open log file [" + logFileName + "] for writing: " + err.Error())
+	}
+	defer fileHandle.Close()
+	util.Logger.Out = fileHandle
 	util.Logger.Info("Version: " + baseConf.AgentVersion)
 
 	//prepare credential
